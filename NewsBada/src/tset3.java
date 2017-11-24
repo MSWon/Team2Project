d1ba88fb3d1bc3c427d91c0e8c9042b36bddba6f
Youngmin Jeon <ktf6599@ajou.ac.kr>
2017-11-24 15:58:26 +0900

    test3

--- NewsBada/src/tset3.java
+++ NewsBada/src/tset3.java
@@ -3,8 +3,8 @@ public class tset3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//dasdasda
 		System.out.print("hello, hello");
+		//class∏∏ commit«œ±‚
 	}
 
 }
